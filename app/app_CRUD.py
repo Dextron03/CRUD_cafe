4efb70febca925a593bb21bc1f605a38d7cc466f
BrailyRs03 <brailyrs03@gmail.com>
2023-10-12 23:36:58 -0400

    He avanzado todo la opcion read, de paso controle varias excepciones que no habia controlado antes.

--- app/app_CRUD.py
+++ app/app_CRUD.py
@@ -1,19 +1,15 @@
 import sqlite3
 from conexion import Conexion
 
-
 class CRUD(Conexion):
     def __init__(self, name_db):
-        """Inicializa una instancia de la clase CRUD.
-
-        Args:
-            name_db (str): El nombre del archivo de base de datos SQLite."""
-        super().__init__(name_db)
+        try:
+            super().__init__(name_db)
+            self.tabla_seleccionada = None
+        except sqlite3.Error as e:
+            print(f"Error al conectar a la base de datos: {e}")
 
     def obtener_decision_seguir(self) -> int:
-        """Permite al usuario elegir si desea continuar con el proceso o salir del CRUD.
-        Returns:
-            int: 1 si el usuario desea continuar, 2 si el usuario desea salir."""
         while True:
             try:
                 seguir = int(input("\n¿Desea seguir con el proceso?\nPara seguir, digite --> 1\nPara salir, digite --> 2\nIngrese su opción: "))
@@ -25,107 +21,139 @@ class CRUD(Conexion):
                 print("La respuesta debe ser un número entero (1 o 2).")
 
     def query_gr_tabla(self):
-        """Retorna la lista de nombres de tablas existentes en la base de datos.
-
-        Returns:
-            list: Una lista de nombres de tablas."""
-        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        return self.cursor.fetchall()
+        try:
+            self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            return self.cursor.fetchall()
+        except sqlite3.Error as e:
+            print(f"Error al obtener las tablas: {e}")
+            return []
 
     def imprimir_tablas(self):
-        """Imprime los nombres de las tablas disponibles en la base de datos."""
         tablas = self.query_gr_tabla()
+        if not tablas:
+            print("No se encontraron tablas en la base de datos.")
+            return
+
         contador = 0
         for tabla in tablas:
-            contador += 1
             print(f"{contador}.{''.join(tabla)}")
+            contador += 1
 
     def funtion_selec_table(self):
-        """Permite al usuario elegir la tabla con la que quiere trabajar.
-
-        Returns:
-            int: El número de la tabla seleccionada."""
         while True:
             try:
-                selec_table = int(input(f"\n¿Que tabla desea consultar? "))
-                if isinstance(selec_table, int):
-                    print(" ")
+                selec_table = int(input(f"\n¿Qué tabla desea consultar? "))
+                if isinstance(selec_table, int) and 0 <= selec_table < len(self.query_gr_tabla()):
+                    self.tabla_seleccionada = selec_table
                     return selec_table
                 else:
                     print("Respuesta inválida, inténtalo de nuevo.")
             except ValueError:
-                indices = [i for i in range(1, 4)]
-                self.imprimir_tablas()
-                print(f"Por favor, ingrese el número/índice ({indices}, etc.).")
+                print("La respuesta debe ser un número entero válido.")
 
     def opcion_crear(self):
-        """Permite al usuario crear un nuevo registro en la tabla seleccionada."""
         print("\nHas elegido Crear.")
         self.imprimir_tablas()
         select_table = self.funtion_selec_table()
 
-        # Crear/Insertar
         print("\nINSERTA LOS DATOS CORRESPONDIENTES:")
         nom_empleado = input("\tIngrese el nombre del empleado: ")
         ape_part_empleado = input(f"\tIngrese el apellido paterno de {nom_empleado}: ")
         ape_mart_empleado = input(f"\tIngrese el apellido materno de {nom_empleado}: ")
         edad_empleado = input(f"\tIngrese la edad del empleado: ")
         salario_empleado = int(input(f"\tIngrese el salario: "))
-        self.cursor.execute(f"INSERT INTO {''.join(self.query_gr_tabla()[0])}(nombre_empleado, apellido_paterno, apellido_materno, edad, salario) VALUES('{nom_empleado}','{ape_part_empleado}','{ape_mart_empleado}','{edad_empleado}', '{salario_empleado}');")
-        self.conn.commit()
-        print(f"Registro insertado.".upper())
-        
-    def buscar_columna(self):    
-        self.cursor.execute(f"PRAGMA table_info({''.join(self.query_gr_tabla()[0])});")
-        print("Que elija los campos que quiera ver:")
+
+        try:
+            self.cursor.execute(f"INSERT INTO {''.join(self.query_gr_tabla()[select_table])}(nombre_empleado, apellido_paterno, apellido_materno, edad, salario) VALUES(?, ?, ?, ?, ?);",
+                                (nom_empleado, ape_part_empleado, ape_mart_empleado, edad_empleado, salario_empleado))
+            self.conn.commit()
+            print(f"Registro insertado.".upper())
+        except (sqlite3.Error, ValueError) as e:
+            print(f"Error al insertar el registro: {e}")
+
+    def buscar_columna(self):
+        select_table = self.funtion_selec_table()
+        self.cursor.execute(f"PRAGMA table_info({''.join(self.query_gr_tabla()[select_table])});")
+        print("Elija los campos que quiera ver:")
         list_colums = self.cursor.fetchall()
-        list_colums.append([len(list_colums), "Salir"])  # Agregar "Salir" al final de la lista
+        list_colums.append([len(list_colums), "Todas"])
         for contador, columna in enumerate(list_colums):
             print(f"{contador}.{columna[1]}")
-        
+
+    def selector_columna(self) -> int:
+        try:
+            select_colum = int(input("\nSegún los índices numéricos.\n¿Qué columnas deseas ver? "))
+            return select_colum
+        except ValueError:
+            print("Respuesta inválida, elija un número válido.")
+            return -1
+
     def opcion_read(self):
         self.imprimir_tablas()
-        select_table = self.funtion_selec_table()
         self.buscar_columna()
+        todas_columnas = []
+        columnas_consultar = []
+        while True:
+            self.cursor.execute(f"PRAGMA table_info({''.join(self.query_gr_tabla()[self.tabla_seleccionada])});")
+            select_colum = self.selector_columna()
+            columnas_tablas: list = self.cursor.fetchall()
 
-            
-        
+            while len(columnas_tablas) > len(todas_columnas):
+                for i in columnas_tablas:
+                    todas_columnas.append(i[1])
 
-        #self.cursor.execute(f"SELECT  FROM {''.join(self.query_gr_tabla()[0])});")
+            if select_colum == 6:
+                columnas_consultar = todas_columnas[:]
+            elif 0 <= select_colum < len(columnas_tablas):
+                columnas_consultar.append(todas_columnas[select_colum])
+            else:
+                print("Respuesta inválida, elija un índice válido.")
+                continue
+
+            if select_colum == 6:
+                break
+            else:
+                decision_seguir = input("¿Quieres agregar más columnas [Y/N]?")
+                if decision_seguir.upper() == "Y":
+                    continue
+                elif decision_seguir.upper() == "N":
+                    break
+
+        query = self.cursor.execute("SELECT " + ", ".join(columnas_consultar) + " FROM " + ''.join(self.query_gr_tabla()[self.tabla_seleccionada]) + ";")
+
+        for i in query.fetchall():
+            print(i)
 
     def opciones(self):
-        """Ejecuta el menú principal del CRUD y gestiona las operaciones disponibles."""
         while True:
             try:
-                opcion = int(input("\n¿Qué quieres hacer hoy?\n1. Crear\n2. Leer\n3. Actualizar\n4. Eliminar\n5. Salir\n6. Buscar\nDijite su opcion: "))
+                opcion = int(input("\n¿Qué quieres hacer hoy?\n1. Crear\n2. Leer\n3. Actualizar\n4. Eliminar\n5. Salir\n6. Buscar\nDijite su opción: "))
             except ValueError:
                 print("Debes elegir un número válido.")
                 continue
 
-            if opcion == 1: #OPCION CREATE
+            if opcion == 1:
                 self.opcion_crear()
                 decision_funcion = self.obtener_decision_seguir()
-                #self.tomar_decision(decision_funcion)
                 if decision_funcion == 1:
                     print("Has elegido seguir.")
                     continue
                 elif decision_funcion == 2:
                     print("Has salido del programa.")
                     break
-                
-            elif opcion == 2: #OPCION LEER
+
+            elif opcion == 2:
                 print("\nHas elegido Leer.".upper())
                 self.opcion_read()
-                
+
                 decision_funcion = self.obtener_decision_seguir()
-                
+
                 if decision_funcion == 1:
                     print("Has elegido seguir.")
                     continue
                 elif decision_funcion == 2:
                     print("Has salido del programa.")
                     break
-                
 
             elif opcion == 3:
                 print("Has elegido Actualizar.")
@@ -135,7 +163,7 @@ class CRUD(Conexion):
 
             elif opcion == 5:
                 print("Saliendo del programa.")
-                self.cerrar_conexion()  # Cierra la conexión antes de salir
+                self.cerrar_conexion()
                 break
             elif opcion == 6:
                 print("Siguiendo con el programa.")
@@ -143,6 +171,9 @@ class CRUD(Conexion):
                 print("Opción no válida. Por favor, elige una opción válida.")
 
 if __name__ == "__main__":
-    db_conex = CRUD('./db/cafe.db')
-    db_conex.abrir_conexion()
-    db_conex.opciones()
+    try:
+        db_conex = CRUD('./db/cafe.db')
+        db_conex.abrir_conexion()
+        db_conex.opciones()
+    except sqlite3.Error as e:
+        print(f"Error al iniciar la aplicación: {e}")
