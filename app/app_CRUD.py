aa6fa6e9c54096dfcdc1e4100fa289d77a5c7770
BrailyRs03 <brailyrs03@gmail.com>
2023-10-15 02:31:12 -0400

    Ahora le impresion de los campos se ven en forma de tabla

--- app/app_CRUD.py
+++ app/app_CRUD.py
@@ -1,31 +1,15 @@
 import sqlite3
 from conexion import Conexion
 from tabulate import tabulate
-
 class CRUD(Conexion):
-    def __init__(self, nombre_db):
-        """
-        Inicializa una instancia de la clase CRUD.
-
-        Parámetros:
-        nombre_db (str): Nombre del archivo de la base de datos.
-
-        Atributos:
-        tabla_seleccionada (int): Almacena el índice de la tabla seleccionada por el usuario.
-        """
+    def __init__(self, name_db):
         try:
-            super().__init__(nombre_db)
+            super().__init__(name_db)
             self.tabla_seleccionada = None
         except sqlite3.Error as e:
             print(f"Error al conectar a la base de datos: {e}")
 
     def obtener_decision_seguir(self) -> int:
-        """
-        Permite al usuario decidir si desea seguir con el proceso o salir.
-
-        Retorna:
-        int: La decisión del usuario (1 para seguir, 2 para salir).
-        """
         while True:
             try:
                 seguir = int(input("\n¿Desea seguir con el proceso?\nPara seguir, digite --> 1\nPara salir, digite --> 2\nIngrese su opción: "))
@@ -36,13 +20,7 @@ class CRUD(Conexion):
             except ValueError:
                 print("La respuesta debe ser un número entero (1 o 2).")
 
-    def consulta_tablas(self):
-        """
-        Recupera el nombre de todas las tablas en la base de datos.
-
-        Retorna:
-        list: Una lista de nombres de tablas en la base de datos.
-        """
+    def query_gr_tabla(self):
         try:
             self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
             return self.cursor.fetchall()
@@ -51,10 +29,7 @@ class CRUD(Conexion):
             return []
 
     def imprimir_tablas(self):
-        """
-        Imprime los nombres de las tablas en la base de datos.
-        """
-        tablas = self.consulta_tablas()
+        tablas = self.query_gr_tabla()
         if not tablas:
             print("No se encontraron tablas en la base de datos.")
             return
@@ -64,17 +39,11 @@ class CRUD(Conexion):
             print(f"{contador}.{''.join(tabla)}")
             contador += 1
 
-    def seleccionar_tabla(self):
-        """
-        Permite al usuario seleccionar una tabla de la base de datos.
-
-        Retorna:
-        int: El índice de la tabla seleccionada por el usuario.
-        """
+    def funtion_selec_table(self):
         while True:
             try:
                 selec_table = int(input(f"\n¿Qué tabla desea consultar? "))
-                if isinstance(selec_table, int) and 0 <= selec_table < len(self.consulta_tablas()):
+                if isinstance(selec_table, int) and 0 <= selec_table < len(self.query_gr_tabla()):
                     self.tabla_seleccionada = selec_table
                     return selec_table
                 else:
@@ -83,12 +52,9 @@ class CRUD(Conexion):
                 print("La respuesta debe ser un número entero válido.")
 
     def opcion_crear(self):
-        """
-        Opción para crear un registro en la tabla seleccionada por el usuario.
-        """
         print("\nHas elegido Crear.")
         self.imprimir_tablas()
-        select_table = self.seleccionar_tabla()
+        select_table = self.funtion_selec_table()
 
         print("\nINSERTA LOS DATOS CORRESPONDIENTES:")
         nom_empleado = input("\tIngrese el nombre del empleado: ")
@@ -98,10 +64,7 @@ class CRUD(Conexion):
         salario_empleado = int(input(f"\tIngrese el salario: "))
 
         try:
-            columns = self.cursor.execute(f"PRAGMA table_info({''.join(self.consulta_tablas()[select_table])});")
-            lista_columnas = columns.fetchall()
-            
-            self.cursor.execute(f"INSERT INTO {''.join(self.consulta_tablas()[select_table])}(nombre_empleado, apellido_paterno, apellido_materno, edad, salario) VALUES(?, ?, ?, ?, ?);",
+            self.cursor.execute(f"INSERT INTO {''.join(self.query_gr_tabla()[select_table])}(nombre_empleado, apellido_paterno, apellido_materno, edad, salario) VALUES(?, ?, ?, ?, ?);",
                                 (nom_empleado, ape_part_empleado, ape_mart_empleado, edad_empleado, salario_empleado))
             self.conn.commit()
             print(f"Registro insertado.".upper())
@@ -109,24 +72,15 @@ class CRUD(Conexion):
             print(f"Error al insertar el registro: {e}")
 
     def buscar_columna(self):
-        """
-        Permite al usuario elegir las columnas a consultar en la tabla seleccionada.
-        """
-        select_table = self.seleccionar_tabla()
-        self.cursor.execute(f"PRAGMA table_info({''.join(self.consulta_tablas()[select_table])});")
+        select_table = self.funtion_selec_table()
+        self.cursor.execute(f"PRAGMA table_info({''.join(self.query_gr_tabla()[select_table])});")
         print("Elija los campos que quiera ver:")
         list_colums = self.cursor.fetchall()
         list_colums.append([len(list_colums), "Todas"])
         for contador, columna in enumerate(list_colums):
             print(f"{contador}.{columna[1]}")
 
-    def seleccionar_columna(self) -> int:
-        """
-        Permite al usuario seleccionar una columna específica para mostrar.
-
-        Retorna:
-        int: El índice de la columna seleccionada.
-        """
+    def selector_columna(self) -> int:
         try:
             select_colum = int(input("\nSegún los índices numéricos.\n¿Qué columnas deseas ver? "))
             return select_colum
@@ -134,17 +88,14 @@ class CRUD(Conexion):
             print("Respuesta inválida, elija un número válido.")
             return -1
 
-    def opcion_leer(self):
-        """
-        Opción para consultar registros en la tabla seleccionada.
-        """
+    def opcion_read(self):
         self.imprimir_tablas()
         self.buscar_columna()
         todas_columnas = []
         columnas_consultar = []
         while True:
-            self.cursor.execute(f"PRAGMA table_info({''.join(self.consulta_tablas()[self.tabla_seleccionada])});")
-            select_colum = self.seleccionar_columna()
+            self.cursor.execute(f"PRAGMA table_info({''.join(self.query_gr_tabla()[self.tabla_seleccionada])});")
+            select_colum = self.selector_columna()
             columnas_tablas: list = self.cursor.fetchall()
 
             while len(columnas_tablas) > len(todas_columnas):
@@ -162,22 +113,18 @@ class CRUD(Conexion):
             if select_colum == 6:
                 break
             else:
-                decision_seguir = input("¿Quieres agregar más columnas [S/N]?")
-                if decision_seguir.upper() == "S":
+                decision_seguir = input("¿Quieres agregar más columnas [Y/N]?")
+                if decision_seguir.upper() == "Y":
                     continue
                 elif decision_seguir.upper() == "N":
                     break
 
-        query = self.cursor.execute("SELECT " + ", ".join(columnas_consultar) + " FROM " + ''.join(self.consulta_tablas()[self.tabla_seleccionada]) + ";")
-        
-        headers = columnas_consultar[:]
-        tabla : tabulate = tabulate(query.fetchall(), headers=headers, tablefmt="fancy_grid")
-        print(tabla)
+        query = self.cursor.execute("SELECT " + ", ".join(columnas_consultar) + " FROM " + ''.join(self.query_gr_tabla()[self.tabla_seleccionada]) + ";")
+
+        table = tabulate(query.fetchall(), headers=columnas_consultar, tablefmt="fancy_grid" )
+        print(table)
 
     def opciones(self):
-        """
-        Menú de opciones para el usuario, que incluye Crear, Leer, Actualizar, Eliminar, Buscar y Salir.
-        """
         while True:
             try:
                 opcion = int(input("\n¿Qué quieres hacer hoy?\n1. Crear\n2. Leer\n3. Actualizar\n4. Eliminar\n5. Salir\n6. Buscar\nDijite su opción: "))
@@ -197,7 +144,7 @@ class CRUD(Conexion):
 
             elif opcion == 2:
                 print("\nHas elegido Leer.".upper())
-                self.opcion_leer()
+                self.opcion_read()
 
                 decision_funcion = self.obtener_decision_seguir()
 
